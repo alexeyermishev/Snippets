02de7923cb85ed108bba6e0626db0b31053909ee
Alexey Ermishev <wilop@mail.ru>
2023-12-17 13:47:42 +0300

    add delete snippet

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
-
+from MainApp.forms import SnippetForm
 
 
 
@@ -11,14 +11,27 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == "GET":
+        form = SnippetForm()
+        context = {
+            'pagename': 'Добавление нового сниппета',
+            'form': form
+        }
+        return render(request, 'pages/add_snippet.html', context)
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippetslist")
+        return render(request, 'pages/add_snippet.html', {'form': form})
+
 
 
 def snippets_page(request):
     snippets = Snippet.objects.all()
     context = {'pagename': 'Просмотр сниппетов',
-                'snippets': snippets}
+                'snippets': snippets,
+                'lensnippets':len(snippets)}
     return render(request, 'pages/view_snippets.html', context)
 
 def get_snippet(request, snippet_id):
@@ -30,3 +43,33 @@ def get_snippet(request, snippet_id):
         "snippet":snippet
     }
     return render(request, 'pages/snippet.html', context)
+
+def create_snippet(request):
+   if request.method == "POST":
+       form = SnippetForm(request.POST)
+       if form.is_valid():
+           form.save()
+           return redirect("snippetslist")
+       return render(request,'pages/add_snippet.html',{'form': form})
+
+def req_del(request, snippet_id):
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f'Snippet with id = {snippet_id} not found.')
+    context = {
+        "snippet":snippet
+    }
+    return render(request, 'pages/snippet_delete.html', context)
+
+
+def delete_snippet(request, snippet_id):
+    snippet = Snippet.objects.get(id=snippet_id)
+    if request.method =="POST":
+        snippet.delete()
+        return redirect('snippetslist')
+ 
+    return render(request, "pages/delete_snippet.html")
+
+
+
